b51820a769e522f4358b2f76c741f33e6e77d7a1
take64 <take64@citrus.tk>
2021-05-04 19:51:17 +0900

    REQUEST_URIに対応

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -68,4 +68,38 @@ class RouterTest extends TestCase
         $this->assertSame($router->action, $action);
         $this->assertSame($router->parameters, $parameters);
     }
+
+
+
+    /**
+     * @test
+     * @throws ConfigureException
+     */
+    public function factory_想定通りAPI()
+    {
+        // 設定値
+        $configures = require(dirname(__DIR__) . '/tests/citrus-configure.php');
+
+        // 生成
+        $router = Router::sharedInstance()->loadConfigures($configures);
+
+        // URLパス設計
+        $_SERVER['REQUEST_URI'] = '/api/user/login';
+        $protocol = 'api';
+        $documents = ['user'];
+        $action = 'login';
+        $parameters = ['email' => 'hoge@example.com'];
+        $request = [
+        ];
+        $request = array_merge($request, $parameters);
+
+        // アイテムの生成
+        $router->factory($request);
+
+        // 検証
+        $this->assertSame($router->protocol, $protocol);
+        $this->assertSame($router->documents, $documents);
+        $this->assertSame($router->action, $action);
+        $this->assertSame($router->parameters, $parameters);
+    }
 }
